0afb9b97dae586b0fef8d129b9eea869682e0767
우유진 <glem1004@gmail.com>
2024-04-02 09:35:42 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "wooyj-1217",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "안드로이드 개발자",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "glem1004@gmail.com",
+    linkedin: "",
+    github: "wooyj-1217",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/wooyj-1217/morethan-log",
     },
   ],
   // blog setting (required)
